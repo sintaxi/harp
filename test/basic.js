a379c2130ffa334dd1fbe174fe7ea992aa19ba78
Brock Whitten <brock@chloi.io>
2013-03-02 18:16:52 -0800

    sets 404 status code on 404 pages

--- test/basic.js
+++ test/basic.js
@@ -18,11 +18,10 @@ describe("basic", function(){
   })  
   
   it("should have global vars", function(done){
-    var globals = require(path.join(outputPath, "globals.json"))
-    globals.should.have.property("environment", "production")
-    globals.should.have.property("public")
+    var staticGlobals = require(path.join(outputPath, "globals.json"))
+    staticGlobals.should.have.property("environment", "production")
+    staticGlobals.should.have.property("public")
     var agent = superagent.agent()
-    
     agent.get('http://localhost:8100/globals.json').end(function(err, rsp){
       rsp.should.have.status(200)
       var dynamicGlobals = JSON.parse(rsp.text)
@@ -33,25 +32,44 @@ describe("basic", function(){
   })
   
   it("should have current vars", function(done){
-    var current = require(path.join(outputPath, "current.json"))
-    current.should.have.property("path")
-    current.should.have.property("source")
-    done()
-  })
-  
-  it("should have index file that uses the layout", function(done){
-    var index = fs.readFile(path.join(outputPath, "index.html"), function(err, contents){
-      contents.toString().should.include("MyHarpApp")
-      contents.toString().should.include("Home")
+    var staticCurrent = require(path.join(outputPath, "current.json"))
+    staticCurrent.should.have.property("path")
+    staticCurrent.should.have.property("source", "current")
+    var agent = superagent.agent()
+    agent.get('http://localhost:8100/current.json').end(function(err, rsp){
+      rsp.should.have.status(200)
+      var dynamicCurrent = JSON.parse(rsp.text)
+      dynamicCurrent.should.have.property("path")
+      dynamicCurrent.should.have.property("source", "current")
       done()
     })
   })
   
+  it("should have index file that uses the layout", function(done){
+    fs.readFile(path.join(outputPath, "index.html"), function(err, contents){
+      contents.toString().should.include("MyHarpApp")
+      contents.toString().should.include("Home")
+      var agent = superagent.agent()
+      agent.get('http://localhost:8100/').end(function(err, rsp){
+        rsp.should.have.status(200)
+        rsp.text.should.include("MyHarpApp")
+        rsp.text.should.include("Home")
+        done()
+      })
+    })
+  })
+  
   it("should have 404 page that does not use layout", function(done){
-    var index = fs.readFile(path.join(outputPath, "404.html"), function(err, contents){
+    fs.readFile(path.join(outputPath, "404.html"), function(err, contents){
       contents.toString().should.not.include("MyHarpApp")
       contents.toString().should.include("My404Page")
-      done()
+      var agent = superagent.agent()
+      agent.get('http://localhost:8100/some-missing-path').end(function(err, rsp){
+        rsp.should.have.status(404)
+        rsp.text.should.not.include("MyHarpApp")
+        rsp.text.should.include("My404Page")
+        done()
+      })
     })
   })
   
