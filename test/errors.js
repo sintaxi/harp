60bfbdf890b9ea01312b55bae80ee08a3eb84779
Brock Whitten <brock@chloi.io>
2013-03-31 15:37:13 -0700

    adds more tests

---
+++ test/errors.js
@@ -0,0 +1,34 @@
+var should      = require("should")
+var superagent  = require('superagent')
+var path        = require("path")
+var fs          = require("fs")
+var exec        = require("child_process").exec
+var harp        = require("../")
+
+describe("errors", function(){
+  var projectPath = path.join(__dirname, "apps/err-invalid-config")
+  var outputPath  = path.join(__dirname, "out/err-invalid-config")
+    
+  before(function(done){
+    harp.server(projectPath, { port: 8101 }, function(){
+      done()
+    })
+  })  
+  
+  it("should get error message for invalid harp.json", function(done){
+    var agent = superagent.agent()
+    agent.get('http://localhost:8101/').end(function(err, rsp){
+      rsp.should.have.status(500)
+      harp.compile(projectPath, outputPath, function(){
+        done()
+      })
+    })
+  })
+  
+  after(function(done){
+    exec("rm -rf " + outputPath, function(){
+      done()
+    })
+  })
+  
+})
