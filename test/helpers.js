2818f795f17ee037f1d2be01dff811fcccdb348f
Brock Whitten <brock@chloi.io>
2013-07-17 17:59:07 -0700

    wrote helpers for checking for colllides

--- test/helpers.js
+++ test/helpers.js
@@ -6,11 +6,28 @@ describe("helpers", function(){
   describe("willCollide(projectPath, outputPath)", function(){
     it("should collide if output path is /", function(done){
       helpers.willCollide("/foo/bar/myproject", "/").should.be.true
+      helpers.willCollide("/foo/bar/myproject/", "/").should.be.true
       done()
     })
 
     it("should not collide if output path is /output", function(done){
       helpers.willCollide("/foo/bar/myproject", "/output").should.be.false
+      helpers.willCollide("/foo/bar/myproject", "/output/").should.be.false
+      helpers.willCollide("/foo/bar/myproject/", "/output/").should.be.false
+      helpers.willCollide("/foo/bar/myproject/", "/output").should.be.false
+      done()
+    })
+
+    it("should not collide if output path is in projectPath", function(done){
+      helpers.willCollide("/foo/bar/myproject", "/foo/bar/myproject/www").should.be.false
+      helpers.willCollide("/foo/bar/myproject/", "/foo/bar/myproject/www").should.be.false
+      helpers.willCollide("/foo/bar/myproject", "/foo/bar/myproject/www/").should.be.false
+      helpers.willCollide("/foo/bar/myproject/", "/foo/bar/myproject/www/").should.be.false
+      done()
+    })
+
+    it("should not collide if project path is one back and begins with underscore", function(done){
+      helpers.willCollide("/foo/bar/myproject", "/foo/bar").should.be.true
       done()
     })
   })
