02256b2c0f1d904c6001cfea79c81f24ed289a71
Brock Whitten <brock@chloi.io>
2013-07-17 17:40:19 -0700

    adds function to check for collision on ouput path

--- test/helpers.js
+++ test/helpers.js
@@ -4,12 +4,15 @@ var helpers = require("../lib/helpers")
 describe("helpers", function(){
 
   describe("willCollide(projectPath, outputPath)", function(){
-
     it("should collide if output path is /", function(done){
       helpers.willCollide("/foo/bar/myproject", "/").should.be.true
       done()
     })
 
+    it("should not collide if output path is /output", function(done){
+      helpers.willCollide("/foo/bar/myproject", "/output").should.be.false
+      done()
+    })
   })
 
 })
