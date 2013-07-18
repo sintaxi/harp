1ac695d869335d88341523e71fd047dd5b633c89
Brock Whitten <brock@chloi.io>
2013-07-17 18:05:19 -0700

    never allow compiling to /

--- test/helpers.js
+++ test/helpers.js
@@ -5,6 +5,7 @@ describe("helpers", function(){
 
   describe("willCollide(projectPath, outputPath)", function(){
     it("should collide if output path is /", function(done){
+      helpers.willCollide("/", "/").should.be.true
       helpers.willCollide("/foo/bar/myproject", "/").should.be.true
       helpers.willCollide("/foo/bar/myproject/", "/").should.be.true
       done()
