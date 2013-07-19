84d22cd4789147071c50bbca715e380895ae7fed
Brock Whitten <brock@chloi.io>
2013-07-18 18:34:22 -0700

    test for 404.html page

--- test/plain.js
+++ test/plain.js
@@ -5,7 +5,7 @@ var fs          = require("fs")
 var exec        = require("child_process").exec
 var harp        = require("../")
 
-describe("basic", function(){
+describe("plain", function(){
   var projectPath = path.join(__dirname, "apps/plain")
   var outputPath  = path.join(__dirname, "out/plain")
 
@@ -21,6 +21,22 @@ describe("basic", function(){
   it("should have node version in config", function(done){
     config.should.have.property("harp_version")
     done()
+  })
+
+  it("should have custom 404 page that is raw HTML", function(done){
+    fs.readFile(path.join(outputPath, "404.html"), function(err, contents){
+      var agent = superagent.agent()
+      agent.get('http://localhost:8102/404.html').end(function(err, rsp){
+        rsp.should.have.status(200)
+        rsp.text.should.eql(contents.toString())
+        agent.get('http://localhost:8102/missing/path').end(function(err, rsp){
+          rsp.should.have.status(404)
+          rsp.text.should.eql(contents.toString())
+          done()
+        })
+        done()
+      })
+    })
   })
 
   after(function(done){
