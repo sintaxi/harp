df4f1a24459b6e1dc49e2ab7776ccc3299058d02
Brock Whitten <brock@chloi.io>
2013-08-19 11:34:00 -0700

    created failing test for issue #72

--- test/plain.js
+++ test/plain.js
@@ -67,6 +67,65 @@ describe("plain", function(){
 
   })
 
+  describe("root-style", function(){
+    var projectPath = path.join(__dirname, "apps/plain/root-style")
+    var outputPath  = path.join(__dirname, "out/plain/root-style")
+
+    var config;
+
+    before(function(done){
+      harp.compile(projectPath, outputPath, function(errors, output){
+        config = output
+        harp.server(projectPath, { port: 8103 }, done)
+      })
+    })
+
+    it("should have node version in config", function(done){
+      config.should.have.property("harp_version")
+      done()
+    })
+
+    it("should serve index file", function(done){
+      fs.readFile(path.join(outputPath, "index.html"), function(err, contents){
+        var agent = superagent.agent()
+        agent.get('http://localhost:8103/').end(function(err, rsp){
+          rsp.should.have.status(200)
+          rsp.text.should.eql(contents.toString())
+          done()
+        })
+      })
+    })
+
+    it("should serve text file", function(done){
+      fs.readFile(path.join(outputPath, "hello.txt"), function(err, contents){
+        var agent = superagent.agent()
+        contents.toString().should.eql("text files are wonderful")
+        agent.get('http://localhost:8103/hello.txt').end(function(err, rsp){
+          rsp.should.have.status(200)
+          rsp.text.should.eql(contents.toString())
+          done()
+        })
+      })
+    })
+
+    it("should have custom 404 page that is raw HTML", function(done){
+      fs.readFile(path.join(outputPath, "404.html"), function(err, contents){
+        var agent = superagent.agent()
+        agent.get('http://localhost:8103/404.html').end(function(err, rsp){
+          rsp.should.have.status(200)
+          rsp.text.should.eql(contents.toString())
+          var agent = superagent.agent()
+          agent.get('http://localhost:8103/missing/path').end(function(err, rsp){
+            rsp.should.have.status(404)
+            rsp.text.should.eql(contents.toString())
+            done()
+          })
+        })
+      })
+    })
+
+  })
+
   after(function(done){
     exec("rm -rf " + output, function(){
       done()
