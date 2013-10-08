a0e05a530f98780dfeaa72dde60d5f96cf6e6bc0
Brock Whitten <brock@chloi.io>
2013-10-08 16:17:09 -0700

    more header tests on error pages

--- test/headers.js
+++ test/headers.js
@@ -13,7 +13,16 @@ describe("headers", function(){
     harp.server(projectPath, { port: port }, done)
   })
 
-  it("should be correct invalid jade file", function(done){
+  it("should be correct with an invalid ejs file", function(done){
+    superagent.agent().get("http://localhost:" + port + "/invalid-ejs.html").end(function(err, rsp){
+      rsp.should.have.status(500)
+      rsp.headers.should.have.property("content-type", "text/html; charset=UTF-8")
+      rsp.headers.should.have.property("content-length")
+      done()
+    })
+  })
+
+  it("should be correct with an invalid jade file", function(done){
     superagent.agent().get("http://localhost:" + port + "/invalid-jade.html").end(function(err, rsp){
       rsp.should.have.status(500)
       rsp.headers.should.have.property("content-type", "text/html; charset=UTF-8")
@@ -22,10 +31,30 @@ describe("headers", function(){
     })
   })
 
-  it("should be correct invalid less file", function(done){
+  it("should be correct with an invalid less file", function(done){
     superagent.agent().get("http://localhost:" + port + "/invalid-less.css").end(function(err, rsp){
       rsp.should.have.status(500)
       rsp.headers.should.have.property("content-type", "text/css; charset=UTF-8")
+      rsp.headers.should.have.property("content-length")
+      done()
+    })
+  })
+
+  it("should be correct with an invalid stylus file", function(done){
+    superagent.agent().get("http://localhost:" + port + "/invalid-styl.css").end(function(err, rsp){
+      rsp.should.have.status(500)
+      rsp.headers.should.have.property("content-type", "text/css; charset=UTF-8")
+      rsp.headers.should.have.property("content-length")
+      done()
+    })
+  })
+
+  // TODO: This should change to javascript error file.
+  //
+  it("should be correct with an invalid CoffeeScript file", function(done){
+    superagent.agent().get("http://localhost:" + port + "/invalid-coffee.js").end(function(err, rsp){
+      rsp.should.have.status(500)
+      rsp.headers.should.have.property("content-type", "text/html; charset=UTF-8")
       rsp.headers.should.have.property("content-length")
       done()
     })
