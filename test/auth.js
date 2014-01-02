c14ba3c97705420138bf991dee78b0a0b6d8541f
Brock Whitten <brock@chloi.io>
2014-01-02 14:26:37 -0800

    writes failing test for basic auth integration

---
+++ test/auth.js
@@ -0,0 +1,23 @@
+var should  = require("should")
+var request = require('request')
+var path    = require('path')
+var harp    = require("../")
+
+describe("basicAuth", function(){
+
+  describe("single", function(done){
+    var projectPath = path.join(__dirname, "apps/auth/single")
+
+    before(function(done){
+      harp.server(projectPath, { port: 8310 }, done)
+    })
+
+    it("should be a protected page", function(done){
+      request('http://localhost:8310/', function (e, r, b) {
+        r.statusCode.should.eql(401)
+        done()
+      })
+    })
+  })
+
+})
