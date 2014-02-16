b86897308673c4d17b58fcaa4237be85d3463446
zeke <zeke@sikelianos.com>
2014-02-15 20:10:07 -0800

    test CLI behavior with nixt, starting with `harp init`

---
+++ test/harp.js
@@ -0,0 +1,52 @@
+var nixt = require('nixt')
+
+describe("harp init", function() {
+
+  beforeEach(function(done){
+    nixt()
+      .run("rm -rf /tmp/harp && mkdir /tmp/harp")
+      .exist("/tmp/harp")
+      .end(done)
+  })
+
+  it("downloads the default boilerplate if it's not set", function(done) {
+    nixt()
+      .run('harp init /tmp/harp')
+      .stdout(/Downloading.*harp-boilerplates\/default/)
+      .stdout(/Initialized project at \/tmp\/harp/)
+      .exist('/tmp/harp/404.jade')
+      .exist('/tmp/harp/_layout.jade')
+      .exist('/tmp/harp/index.jade')
+      .exist('/tmp/harp/main.less')
+      .end(done)
+  })
+
+  it("defaults to the harp-boilerplates github org when given a shorthand pattern", function(done) {
+    nixt()
+      .run('harp init /tmp/harp -b hb-start')
+      .stdout(/Downloading.*harp-boilerplates\/hb-start/)
+      .exist('/tmp/harp/public')
+      .end(done)
+  })
+
+  it("honors -b option when given a user/repo pattern", function(done) {
+    nixt()
+      .run('harp init /tmp/harp -b zeke/harp-sample')
+      .stdout(/Downloading.*zeke\/harp-sample/)
+      .exist('/tmp/harp/README.md')
+      .exist('/tmp/harp/index.jade')
+      .end(done)
+  })
+
+  it("doesn't overwrite an existing directory", function(done) {
+    nixt()
+      .run('harp init /tmp/harp')
+      .end(function() {
+        nixt()
+          .run('harp init /tmp/harp')
+          .stdout(/Sorry,.*must be empty/)
+          .end(done)
+      })
+  })
+
+})
