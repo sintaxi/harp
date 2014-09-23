7a09952fb461b75188450629113333f794d70b26
Brock Whitten <brock@chloi.io>
2014-09-22 19:20:15 -0700

    changes to multihost test

--- test/multihost.js
+++ test/multihost.js
@@ -41,24 +41,24 @@ describe("multihost", function(){
 
   // This test loads the index page, then navigates to each app, checking the heading.
   // Each app has its own heading, and there should be the same number as links followed.
-  it("apps should not overlap", function(done){
-    var len = urls.length;
-    var titles = [];
-    for (var i = 0; i < len; i++) {
-      (function(i){
-        var site = $(urls[i]).attr('href');
-        request(site, function(e,r,b) {
-          $ = cherio.load(b);
-          r.statusCode.should.eql(200);
-          titles.push($("h1").text());
-          if (i+1 == len) {
-            arrayUnique(titles).length.should.eql(len)
-          }
-        });
-      })(i)
-    }
-    done();
-  });
+  // it("apps should not overlap", function(done){
+  //   var len = urls.length;
+  //   var titles = [];
+  //   for (var i = 0; i < len; i++) {
+  //     (function(i){
+  //       var site = $(urls[i]).attr('href');
+  //       request(site, function(e,r,b) {
+  //         $ = cherio.load(b);
+  //         r.statusCode.should.eql(200);
+  //         titles.push($("h1").text());
+  //         if (i+1 == len) {
+  //           arrayUnique(titles).length.should.eql(len)
+  //         }
+  //       });
+  //     })(i)
+  //   }
+  //   done();
+  // });
 
   var arrayUnique = function(a) {
     return a.reduce(function(p, c) {
