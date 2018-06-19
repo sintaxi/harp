153f495d9369c2083883e30be4029eb408391473
Brock Whitten <brock@sintaxi.com>
2018-06-19 02:48:52 -0700

    version bump v0.27.0 adds bundle.cjs support

--- test/apps/cjs/root/bundle.cjs
+++ test/apps/cjs/root/bundle.cjs
@@ -1,2 +1,4 @@
 
-console.log("hello world")
+var div = document.createElement("h1")
+div.appendChild(document.createTextNode("hello world"))
+document.body.appendChild(div)
--- test/cjs.js
+++ test/cjs.js
@@ -7,9 +7,40 @@ var harp        = require("../");
 
 describe("cjs", function(){
 
-  describe("basic app", function(){
+  describe("basic root project", function(){
     var projectPath = path.join(__dirname, "apps/cjs/root")
     var outputPath  = path.join(__dirname, "out/cjs-root")
+
+    it("should compile", function(done){
+      harp.compile(projectPath, outputPath, function(error){
+        should.not.exist(error)
+        done()
+      })
+    })
+
+    it("compile should not include bundle.cjs", function(done) {
+
+      var rsp = fs.existsSync(path.join(outputPath, "/bundle.cjs"))
+      rsp.should.be.false
+
+      var rsp = fs.existsSync(path.join(outputPath, "/bundle.js"))
+      rsp.should.be.true
+
+      done()
+
+    })
+
+    after(function(done){
+      exec("rm -rf " + outputPath, function() {
+        done();
+      })
+    })
+
+  })
+
+  describe("basic framework project", function(){
+    var projectPath = path.join(__dirname, "apps/cjs/framework")
+    var outputPath  = path.join(__dirname, "out/cjs-framework")
 
     it("should compile", function(done){
       harp.compile(projectPath, outputPath, function(error){
