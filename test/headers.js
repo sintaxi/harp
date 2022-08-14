7f43ebddd9015742b9ca14da17ca039ddd80fb75
Brock Whitten <brock@sintaxi.com>
2022-08-13 17:11:57 -0700

    uses axios in test/headers file

--- test/headers.js
+++ test/headers.js
@@ -1,5 +1,5 @@
 var should      = require("should")
-var request     = require('request')
+var axios       = require('axios')
 var path        = require('path')
 var harp        = require('../')
 
@@ -17,8 +17,8 @@ describe("headers", function(){
   // static
 
   it("should be correct with a valid CSS file", function(done){
-    request("http://localhost:" + port + "/valid-css.css", function(e,r,b){
-      r.statusCode.should.eql(200)
+    axios.get("http://localhost:" + port + "/valid-css.css").then(function(r){
+      r.status.should.eql(200)
       r.headers.should.have.property("content-type", "text/css; charset=UTF-8")
       r.headers.should.have.property("content-length")
       done()
@@ -26,8 +26,8 @@ describe("headers", function(){
   })
 
   it("should be correct with a valid HTML file", function(done){
-    request("http://localhost:" + port + "/valid-html.html", function(e, r, b){
-      r.statusCode.should.eql(200)
+    axios.get("http://localhost:" + port + "/valid-html.html").then(function(r){
+      r.status.should.eql(200)
       r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
       r.headers.should.have.property("content-length")
       done()
@@ -35,8 +35,8 @@ describe("headers", function(){
   })
 
   it("should be correct with a valid JS file", function(done){
-    request("http://localhost:" + port + "/valid-js.js", function(e, r, b){
-      r.statusCode.should.eql(200)
+    axios.get("http://localhost:" + port + "/valid-js.js").then(function(r){
+      r.status.should.eql(200)
       r.headers.should.have.property("content-type", "application/javascript; charset=UTF-8")
       r.headers.should.have.property("content-length")
       done()
@@ -46,8 +46,8 @@ describe("headers", function(){
   // valid
 
   it("should be correct with a valid Jade file", function(done){
-    request("http://localhost:" + port + "/valid-jade.html", function(e, r, b){
-      r.statusCode.should.eql(200)
+    axios.get("http://localhost:" + port + "/valid-jade.html").then(function(r){
+      r.status.should.eql(200)
       r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
       r.headers.should.have.property("content-length")
       done()
@@ -55,8 +55,8 @@ describe("headers", function(){
   })
 
   it("should be correct with a valid EJS file", function(done){
-    request("http://localhost:" + port + "/valid-ejs.html", function(e, r, b){
-      r.statusCode.should.eql(200)
+    axios.get("http://localhost:" + port + "/valid-ejs.html").then(function(r){
+      r.status.should.eql(200)
       r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
       r.headers.should.have.property("content-length")
       done()
@@ -64,8 +64,8 @@ describe("headers", function(){
   })
 
   it("should be correct with a valid Markdown file", function(done){
-    request("http://localhost:" + port + "/valid-markdown.html", function(e, r, b){
-      r.statusCode.should.eql(200)
+    axios.get("http://localhost:" + port + "/valid-markdown.html").then(function(r){
+      r.status.should.eql(200)
       r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
       r.headers.should.have.property("content-length")
       done()
@@ -100,8 +100,8 @@ describe("headers", function(){
   // })
 
   it("should be correct with a valid SCSS file", function(done){
-    request("http://localhost:" + port + "/valid-scss.css", function(e, r, b){
-      r.statusCode.should.eql(200)
+    axios.get("http://localhost:" + port + "/valid-scss.css").then(function(r){
+      r.status.should.eql(200)
       r.headers.should.have.property("content-type", "text/css; charset=UTF-8")
       r.headers.should.have.property("content-length")
       done()
@@ -109,8 +109,8 @@ describe("headers", function(){
   })
 
   it("should be correct with a valid Sass file", function(done){
-    request("http://localhost:" + port + "/valid-sass.css", function(e, r, b){
-      r.statusCode.should.eql(200)
+    axios.get("http://localhost:" + port + "/valid-sass.css").then(function(r){
+      r.status.should.eql(200)
       r.headers.should.have.property("content-type", "text/css; charset=UTF-8")
       r.headers.should.have.property("content-length")
       done()
@@ -120,19 +120,19 @@ describe("headers", function(){
   // invalid
 
   it("should be correct with an invalid EJS file", function(done){
-    request("http://localhost:" + port + "/invalid-ejs.html", function(e, r, b){
-      r.statusCode.should.eql(500)
-      r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
-      r.headers.should.have.property("content-length")
+    axios.get("http://localhost:" + port + "/invalid-ejs.html").catch(function(e){
+      e.response.status.should.eql(500)
+      e.response.headers.should.have.property("content-type", "text/html; charset=UTF-8")
+      e.response.headers.should.have.property("content-length")
       done()
     })
   })
 
   it("should be correct with an invalid Jade file", function(done){
-    request("http://localhost:" + port + "/invalid-jade.html", function(e, r, b){
-      r.statusCode.should.eql(500)
-      r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
-      r.headers.should.have.property("content-length")
+    axios.get("http://localhost:" + port + "/invalid-jade.html").catch(function(e){
+      e.response.status.should.eql(500)
+      e.response.headers.should.have.property("content-type", "text/html; charset=UTF-8")
+      e.response.headers.should.have.property("content-length")
       done()
     })
   })
@@ -156,8 +156,8 @@ describe("headers", function(){
   // })
 
   it("should be correct with an invalid SCSS file", function(done){
-    request("http://localhost:" + port + "/invalid-scss.css", function(e, r, b){
-      r.statusCode.should.eql(200)
+    axios.get("http://localhost:" + port + "/invalid-scss.css").then(function(r){
+      r.status.should.eql(200)
       r.headers.should.have.property("content-type", "text/css; charset=UTF-8")
       r.headers.should.have.property("content-length")
       done()
@@ -165,8 +165,8 @@ describe("headers", function(){
   })
 
   it("should be correct with an invalid Sass file", function(done){
-    request("http://localhost:" + port + "/invalid-sass.css", function(e, r, b){
-      r.statusCode.should.eql(200)
+    axios.get("http://localhost:" + port + "/invalid-sass.css").then(function(r){
+      r.status.should.eql(200)
       r.headers.should.have.property("content-type", "text/css; charset=UTF-8")
       r.headers.should.have.property("content-length")
       done()
@@ -186,64 +186,64 @@ describe("headers", function(){
   // direct
 
   it("should be correct when Jade file requested", function(done){
-    request("http://localhost:" + port + "/valid-jade.jade", function(e, r, b){
-      r.statusCode.should.eql(404)
-      r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
-      r.headers.should.have.property("content-length")
+    axios.get("http://localhost:" + port + "/valid-jade.jade").catch(function(e){
+      e.response.status.should.eql(404)
+      e.response.headers.should.have.property("content-type", "text/html; charset=UTF-8")
+      e.response.headers.should.have.property("content-length")
       done()
     })
   })
 
   it("should be correct when EJS file requested", function(done){
-    request("http://localhost:" + port + "/valid-ejs.ejs", function(e, r, b){
-      r.statusCode.should.eql(404)
-      r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
-      r.headers.should.have.property("content-length")
+    axios.get("http://localhost:" + port + "/valid-ejs.ejs").catch(function(e){
+      e.response.status.should.eql(404)
+      e.response.headers.should.have.property("content-type", "text/html; charset=UTF-8")
+      e.response.headers.should.have.property("content-length")
       done()
     })
   })
 
   it("should be correct when Markdown file requested", function(done){
-    request("http://localhost:" + port + "/valid-markdown.md", function(e, r, b){
-      r.statusCode.should.eql(404)
-      r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
-      r.headers.should.have.property("content-length")
+    axios.get("http://localhost:" + port + "/valid-markdown.md").catch(function(e){
+      e.response.status.should.eql(404)
+      e.response.headers.should.have.property("content-type", "text/html; charset=UTF-8")
+      e.response.headers.should.have.property("content-length")
       done()
     })
   })
 
   it("should be correct when CoffeeScript file requested", function(done){
-    request("http://localhost:" + port + "/valid-coffee.coffee", function(e, r, b){
-      r.statusCode.should.eql(404)
-      r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
-      r.headers.should.have.property("content-length")
+    axios.get("http://localhost:" + port + "/valid-coffee.coffee").catch(function(e){
+      e.response.status.should.eql(404)
+      e.response.headers.should.have.property("content-type", "text/html; charset=UTF-8")
+      e.response.headers.should.have.property("content-length")
       done()
     })
   })
 
   it("should be correct when LESS file requested", function(done){
-    request("http://localhost:" + port + "/valid-less.less", function(e, r, b){
-      r.statusCode.should.eql(404)
-      r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
-      r.headers.should.have.property("content-length")
+    axios.get("http://localhost:" + port + "/valid-less.less").catch(function(e){
+      e.response.status.should.eql(404)
+      e.response.headers.should.have.property("content-type", "text/html; charset=UTF-8")
+      e.response.headers.should.have.property("content-length")
       done()
     })
   })
 
   it("should be correct when Stylus file requested", function(done){
-    request("http://localhost:" + port + "/valid-styl.styl", function(e, r, b){
-      r.statusCode.should.eql(404)
-      r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
-      r.headers.should.have.property("content-length")
+    axios.get("http://localhost:" + port + "/valid-styl.styl").catch(function(e){
+      e.response.status.should.eql(404)
+      e.response.headers.should.have.property("content-type", "text/html; charset=UTF-8")
+      e.response.headers.should.have.property("content-length")
       done()
     })
   })
 
   it("should be correct when SCSS file requested", function(done){
-    request("http://localhost:" + port + "/valid-scss.scss", function(e, r, b){
-      r.statusCode.should.eql(404)
-      r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
-      r.headers.should.have.property("content-length")
+    axios.get("http://localhost:" + port + "/valid-scss.scss").catch(function(e){
+      e.response.status.should.eql(404)
+      e.response.headers.should.have.property("content-type", "text/html; charset=UTF-8")
+      e.response.headers.should.have.property("content-length")
       done()
     })
   })
@@ -251,28 +251,28 @@ describe("headers", function(){
   // missing pages
 
   it("should be correct when missing css file", function(done){
-    request("http://localhost:" + port + "/missing.css", function(e, r, b){
-      r.statusCode.should.eql(404)
-      r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
-      r.headers.should.have.property("content-length")
+    axios.get("http://localhost:" + port + "/missing.css").catch(function(e){
+      e.response.status.should.eql(404)
+      e.response.headers.should.have.property("content-type", "text/html; charset=UTF-8")
+      e.response.headers.should.have.property("content-length")
       done()
     })
   })
 
   it("should be correct when missing html file", function(done){
-    request("http://localhost:" + port + "/missing.html", function(e, r, b){
-      r.statusCode.should.eql(404)
-      r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
-      r.headers.should.have.property("content-length")
+    axios.get("http://localhost:" + port + "/missing.html").catch(function(e){
+      e.response.status.should.eql(404)
+      e.response.headers.should.have.property("content-type", "text/html; charset=UTF-8")
+      e.response.headers.should.have.property("content-length")
       done()
     })
   })
 
   it("should be correct when missing js file", function(done){
-    request("http://localhost:" + port + "/missing.js", function(e, r, b){
-      r.statusCode.should.eql(404)
-      r.headers.should.have.property("content-type", "text/html; charset=UTF-8")
-      r.headers.should.have.property("content-length")
+    axios.get("http://localhost:" + port + "/missing.js").catch(function(e){
+      e.response.status.should.eql(404)
+      e.response.headers.should.have.property("content-type", "text/html; charset=UTF-8")
+      e.response.headers.should.have.property("content-length")
       done()
     })
   })
